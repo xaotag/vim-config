46720f73fec1eabb8857caae1d99810d0b29e19c
xaotag <3036905349@qq.com>
2020-10-23 13:02:51 +0800

    manjaro

--- install.sh
+++ install.sh
@@ -2,11 +2,15 @@
 yay -S curl git 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+cd ~ 
 git clone https://github.com/xaotag/vim-config/
 cd vim-config
 ./install.sh
 mkdir ~/.config/nvim
-touch init.yaml
+cd ~/.config/nvim
+touch init.vim
 ln -s ~/.config/nvim ~/.vimrc
+rm -r  ~/vim-config
 
 
