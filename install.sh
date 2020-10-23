59caefb82150b11adef9b7327c97f7edd1e3ab30
xaotag <3036905349@qq.com>
2020-10-23 12:56:37 +0800

    初步完成

--- install.sh
+++ install.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-curl 
+yay -S curl git 
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+git clone https://github.com/xaotag/vim-config/
+cd vim-config
+./install.sh
+mkdir ~/.config/nvim
+touch init.yaml
+ln -s ~/.config/nvim ~/.vimrc
+
+
