f6d0fa84b445ed4ff958418ece685b1a16e79d17
xaotag <xaotag@gmail.com>
2021-02-15 13:38:53 +0800

    change file

--- install.sh
+++ install.sh
@@ -4,9 +4,8 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 cd ~ 
 cd vim-config
-mkdir ~/.config/nvim
-cd ~/.config/nvim
-touch init.vim
+mkdir -p ~/.config/nvim
+cp ./init.vim ~/.config/nvim
 ln -s ~/.config/nvim/init.vim ~/.vimrc
 rm -r  ~/vim-config
 
