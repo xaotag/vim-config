36545c34bab4ebc38c652c8e4959df5aa6425392
xaotag <xaotag@gmail.com>
2021-02-15 13:37:13 +0800

    upload init.vim & install script

--- install.sh
+++ install.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-yay -S curl git 
+yay -Syu curl git 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
+https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 cd ~ 
-git clone https://github.com/xaotag/vim-config/
 cd vim-config
-./install.sh
 mkdir ~/.config/nvim
 cd ~/.config/nvim
 touch init.vim
-ln -s ~/.config/nvim ~/.vimrc
+ln -s ~/.config/nvim/init.vim ~/.vimrc
 rm -r  ~/vim-config
 
 
