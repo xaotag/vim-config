2e8129a347c9550733f5d7b8583b5a61bf822898
xaotag <xaotag@gmail.com>
2021-03-01 21:23:24 +0800

    change install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-yay -Syu curl git 
 cd ~ 
 git clone https://github.com/xaotag/vim-config
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
@@ -7,3 +6,4 @@ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 cd vim-config
 mkdir -p ~/.config/nvim
 cp ./init.vim ~/.config/nvim
+cp ./coc-settings.json ~/.config/nvim
